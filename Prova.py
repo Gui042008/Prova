e7d4cc396a58f8858a82d71ff36d05d2daee0d06
Guilherme <guivjjig@gmail.com>
2025-03-14 11:28:11 -0300

    oiuioo

--- Prova.py
+++ Prova.py
@@ -1,16 +1,18 @@
 custos = [900, 350, 300, 400]
 ganhos = [2500, 500, 1200]
 
-
 def somatorio(lista):
-    return sum(lista)
+    total = 0
+    i = 0
+    while i < len(lista):
+        total += lista[i]
+        i += 1
+    return total
 
 total_custos = somatorio(custos)
 total_ganhos = somatorio(ganhos)
 
-
 diferenca = total_ganhos - total_custos
-
 
 print(f"Total de custos: R${total_custos}")
 print(f"Total de ganhos: R${total_ganhos}")
