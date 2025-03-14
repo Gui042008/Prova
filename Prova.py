cfee1edc0ff60953685adc0291470e71ae10d870
Guilherme <guivjjig@gmail.com>
2025-03-14 11:08:23 -0300

    first commit

---
+++ Prova.py
@@ -0,0 +1,17 @@
+custos = [900, 350, 300, 400]
+ganhos = [2500, 500, 1200]
+
+
+def somatorio(lista):
+    return sum(lista)
+
+total_custos = somatorio(custos)
+total_ganhos = somatorio(ganhos)
+
+
+diferenca = total_ganhos - total_custos
+
+
+print(f"Total de custos: R${total_custos}")
+print(f"Total de ganhos: R${total_ganhos}")
+print(f"Diferença (ganhos - custos): R${diferenca}")
